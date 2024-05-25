ed247e74183762e5c41c8b1cd72dfff14bbb85b4
umarbsse <umarbsse@gmail.com>
2024-05-25 13:05:51 -0700

    Redux App Skeletion done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Shop from './components/Shop';
+import{BrowserRouter,
+  Routes,
+  Route,
+  }from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar/>    
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+        <h2>Buy Adidas shows 100 USD</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Add to cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
